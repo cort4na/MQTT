0f9768b9b7b6f655f8b940406a0965a462d2778d
Rainto <Julia.Rainto@seamk.fi>
2018-02-13 13:25:51 +0200

    Changes added

--- TemperatureMqttPublisher/Program.cs
+++ TemperatureMqttPublisher/Program.cs
@@ -23,7 +23,7 @@ namespace TemperatureMqttPublisher
 
             for (int i = 0; i < 100; i++)
             {
-                double temperature = 20 + 5 * Math.Sin(i / 10.0);
+                double temperature = 28 + 5 * Math.Sin(i / 10.0);
                 string strValue = Convert.ToString(temperature);
 
                 myClient.Publish("measurements/temperature", Encoding.UTF8.GetBytes(strValue));
